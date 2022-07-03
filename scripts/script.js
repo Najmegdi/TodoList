88f694a65d3ba2793520c2c514b6e84ecbbd7299
Najme gdi <najme.gdi@gmail.com>
2022-07-03 18:12:30 +0430

    add design and dark/light theme

---
+++ scripts/script.js
@@ -0,0 +1,27 @@
+document.querySelector('#theme-switch')
+    .addEventListener('change', (e) => {
+        if (e.target.checked) {
+            // document.documentElement = <html>
+            // in daghighan be tage html eshare mikone
+
+            document.documentElement.classList.add('dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+        }
+    });
+
+// man dige bayad beram.
+// fagaht shoma ye zahmati bekeshid. proje
+// ro bezaridesh roye github ke betoonim hardotamoon chizi
+// behesh ezafe konim.
+// chon gharare ke add list va create item ro piade sazish konam ke shoma styleesh ro bezanid
+//ok hatman. mersi emrozo koli vaght gozashtin
+// khahesh mikonam. motmaenam ke arzeshesho dare.
+// natijeii ke mibinam rezayat bakhshe. kheyli khoob pish mirid
+// mamnonam, kami babate nadonestane linkha khodamo sarzanesh kardam, eshala ro sefideton konam
+// ina chizaye koochikie. niazi be sarzanesh nadare. dar asare tajrobe hameye ina bartaraf mishe
+// man revale pishraft o giraii ro mibinam. ta alan harchi kar kardim ro khoob darid be kar mibarid.
+// ye meghdari faghat baazi jaha sooe tafahom shode ke be khatere ine ke darim tooye chat o remote in karo anjam midim
+// ke tooye jalasti mese emrooz bar tarafeshoon mikonim ke hame chiz hal beshe vasatoon
+// dar kol be nazaram ba hoosh o ba estedadid. kheyli aalie in. kheiliii mamnonam babate roheye va nagize ke midin kheili moasere. mamnonam bazam
+// khahesh mikonam. movafagh bashid. felan khodafez bye
